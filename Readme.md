3aef8027862adfddb2ea23553a866b4d8759d0de
K12TK013A1N <shourya15singh7@gmail.com>
2024-08-08 23:49:31 +0530

    image-sharpening done.

--- Readme.md
+++ Readme.md
@@ -4,3 +4,7 @@
 + added padding as when a kernel is passed through a "HxW" matrix it shorts it to "H-2 X W-2", so a padding can change matrix to "H+2 X W+2".
 #### Step-2
 + smoothening can be done when a (3x3) matrix of (1/9s) are passed over the padded image.
+#### Step-3 
++ details can be extracted when smoothened image matrix is substracted from original matrix, keeping in mind that it should be between 0-255.
+#### Step-4 
++ final sharpened image can be aquired by adding detailed matrix to original image.
--- assignment-1/src/image_sharpener.cpp
+++ assignment-1/src/image_sharpener.cpp
@@ -37,7 +37,6 @@ struct image_t* return_padded_image(struct image_t *input_image){ // returns pad
 
 struct image_t* S1_smoothen(struct image_t *input_image)
 {
-	// TODO
 	// remember to allocate space for smoothened_image. See read_ppm_file() in libppm.c for some help.
 	uint8_t*** original_image_matrix = input_image->image_pixels; //extract matrix from original image
 	int original_height = input_image->height; // slly extract height
@@ -52,13 +51,16 @@ struct image_t* S1_smoothen(struct image_t *input_image)
 	for(int i=1; i<original_height-1; i++){
 		for(int j=1; j<original_width-1; j++){
 			for(int k=0; k<3; k++){
-				uint8_t new_value = 0;
+				int new_value = 0;
 				for(int x=-1; x<=1; x++){ //thru row above to row below
 					for(int y=-1; y<=1; y++){ // thru column left to column right
 						new_value += (original_image_matrix[i+x][j+y][k])/9; //add up all 9 val.
 					}
 				}
-				smooth_image_matrix[i-1][j-1][k] = new_value; //update new value
+				if(new_value>255){ // value must be constrained from 0,255
+					new_value = 255;
+				}
+				smooth_image_matrix[i-1][j-1][k] = uint8_t(new_value); //update new value must be type_casted to unsigned 8 bit format
 			}
 		}
 	}
@@ -71,14 +73,70 @@ struct image_t* S1_smoothen(struct image_t *input_image)
 
 struct image_t* S2_find_details(struct image_t *input_image, struct image_t *smoothened_image)
 {
-	// TODO
-	return 0;
+	// let's extract some details from args
+	uint8_t*** original_image_matrix = input_image->image_pixels;
+	uint8_t*** smoothened_image_matrix = smoothened_image->image_pixels;
+	int height = input_image->height;
+	int width = input_image->width;
+	// let's allocate space
+	uint8_t*** detail_image_matrix = new uint8_t**[height];
+	for(int i=0; i<height; i++){
+		detail_image_matrix[i] = new uint8_t*[width];
+		for(int j=0; j<width; j++){
+			detail_image_matrix[i][j] = new uint8_t[3];
+		}
+	}
+	// now for M(details) = M(original) - M(smoothened)
+	for(int i=0;i<height; i++){
+		for(int j=0;j<width; j++){
+			for(int k=0; k<3; k++){
+				int new_value = original_image_matrix[i][j][k] - smoothened_image_matrix[i][j][k];
+				if(new_value<0){ // value must be constrained between 0,255
+					new_value = 0;
+				}
+				detail_image_matrix[i][j][k] = uint8_t(new_value);
+			}
+		}
+	}
+	struct image_t* detail_image = new struct image_t;
+	detail_image->height = height;
+	detail_image->width = width;
+	detail_image->image_pixels = detail_image_matrix;
+	return detail_image;
 }
 
 struct image_t* S3_sharpen(struct image_t *input_image, struct image_t *details_image)
 {
-	// TODO
-	return input_image; //TODO remove this line when adding your code
+	// again collecting info from args
+	uint8_t*** original_image_matrix = input_image->image_pixels;
+	uint8_t*** detail_image_matrix = details_image->image_pixels;
+	int height = input_image->height;
+	int width = input_image->width;
+	// allocating space for sharpen-image-matrix
+	uint8_t*** sharpen_image_matrix = new uint8_t** [height];
+	for(int i=0; i<height; i++){
+		sharpen_image_matrix[i] = new uint8_t*[width];
+		for(int j=0; j<width; j++){
+			sharpen_image_matrix[i][j] = new uint8_t[3];
+		}
+	}
+	// M(sharpened-image) = M(detailed-image) + M(original-image)
+	for(int i=0; i<height; i++){
+		for(int j=0; j<width; j++){
+			for(int k=0; k<3; k++){
+				int value = (original_image_matrix[i][j][k]) + detail_image_matrix[i][j][k];
+				if(value>255){ // value must be constrained between 0,255
+					value = 255;
+				}
+				sharpen_image_matrix[i][j][k] = uint8_t(value);
+			}
+		}
+	}
+	struct image_t* sharpen_image = new struct image_t;
+	sharpen_image->height = height;
+	sharpen_image->width = width;
+	sharpen_image->image_pixels = sharpen_image_matrix;
+	return sharpen_image;
 }
 
 int main(int argc, char **argv)
@@ -91,15 +149,15 @@ int main(int argc, char **argv)
 	
 	struct image_t *input_image = read_ppm_file(argv[1]);
 
-	// cout << input_image->height;
 	struct image_t *padded_image = return_padded_image(input_image);
+
 	struct image_t *smoothened_image = S1_smoothen(padded_image);
 	
-	// struct image_t *details_image = S2_find_details(input_image, smoothened_image);
+	struct image_t *details_image = S2_find_details(input_image, smoothened_image);
 	
-	// struct image_t *sharpened_image = S3_sharpen(input_image, details_image);
+	struct image_t *sharpened_image = S3_sharpen(input_image, details_image);
 	
-	write_ppm_file(argv[2], smoothened_image);
+	write_ppm_file(argv[2], sharpened_image);
 	
 	return 0;
 }
