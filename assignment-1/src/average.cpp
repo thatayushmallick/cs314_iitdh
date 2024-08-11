108aff47d40c206c0b6c5e06f5adc1aea396ca4b
K12TK013A1N <shourya15singh7@gmail.com>
2024-08-11 15:19:24 +0530

    updated averaging method

--- assignment-1/src/average.cpp
+++ assignment-1/src/average.cpp
@@ -5,23 +5,25 @@
 #include <vector>
 using namespace std;
 int main(){
+// Read input.txt
   ifstream input_file("input.txt");
   if(!input_file.is_open()){
     cerr << "ERR OPENING FILE!" << endl;
     return 1;
   }
+  // make a dynamic array to store the average time for each of 5 phases i.e read smooth detail sharpen write
   vector <double> v(5,0);
   string line;
   int i = 0;
   while(getline(input_file, line)){
-    double val = stod(line);
-    v[i%5] += val;
+    double val = stod(line); //convert string to double
+    v[i%5] += val; //adds the value
     i++;
   }
-  i = (i+1)/5;
+  i = (i)/5; //number of iteration for which each experiment was iterated
   for(auto j: v){
-    j = j/(i);
-    cout << j << "\n";
+    j = j/(i); //averaging each value by dividing sum by number of iteration
+    cout << j << "\n"; //output the value for each phase
   }
   input_file.close();
   return 0;
--- assignment-1/src/script.sh
+++ assignment-1/src/script.sh
@@ -6,24 +6,24 @@ fi
 
 g++ -g image_sharpener.cpp libppm.cpp
 
-declare -a average=(
-  [0]=0
-  [1]=0
-  [2]=0
-  [3]=0
-  [4]=0
-)
+if test -f avg.txt;then
+  rm ./avg.txt
+fi
 
 for num in {1..7}
 do
+if test -f input.txt;then
+  rm ./input.txt
+fi
+for x in {1..5}
+do
 ./a.out ../images/$num.ppm result.ppm | cut -d ' ' -f 4 >> input.txt
 done
-
 g++ average.cpp -o avg
 somevar=$(./avg)
-echo "AVERAGE TIME ARE: " >> input.txt
-echo $somevar >> input.txt
-
+echo "AVERAGE TIME FOR FILE $num ARE: " >> avg.txt
+echo $somevar >> avg.txt
 if test -f ./avg;then
   rm ./avg
 fi
+done
